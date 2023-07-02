e9abddcd9c6df2c406bb4ca6f1c4ef2ece7d8579
AvresCode <servas230@gmail.com>
2023-07-02 11:51:27 +0200

    add email validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,37 @@
 import { useForm } from 'react-hook-form';
 import { motion } from 'framer-motion';
+import { useEffect } from 'react';
 
 export const Contact = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    formState,
+    trigger,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful },
+  } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (e) => {
+    // console.log('~ e', e);
+    // console.log('errors', errors);
+
+    const isValid = await trigger();
+
+    if (!isValid) {
+      e.preventDefault();
+    }
+
+    // reset();
   };
+
+  // useEffect(() => {
+  //   if (formState.isSubmitSuccessful) {
+  //     console.log('isSubmitSuccessful',isSubmitSuccessful)
+  //     reset({ name: '', email: '', subject: '', message: '' });
+  //     console.log('formState', formState);
+  //   }
+  // }, [formState, reset]);
 
   return (
     <section
@@ -17,35 +42,61 @@ export const Contact = () => {
         <h1 className="text-4xl p-5 font-playfair">Contact Me</h1>
         <form
           className="text-black flex flex-col gap-2"
-          onSubmit={handleSubmit(onSubmit)}
-          target="_blank"
-          action=""
+          onSubmit={onSubmit}
+          // target="_blank"
+          action="https://formsubmit.co/64028c3f488ace2d11097b65eb3c823f"
           method="POST"
         >
           <div className="flex gap-2">
             <input
-              {...register('Name')}
+              {...register('name', { required: true, maxLength: 20 })}
               placeholder="Name"
               className="w-1/2 p-2 rounded-lg"
             />
+            {errors.name && (
+              <p className="text-rose-500 mt-1">
+                {errors.name.type === 'required' && 'This field is required.'}
+              </p>
+            )}
             <input
-              {...register('Email')}
+              {...register('email', {
+                required: true,
+                pattern: /^[\w.%+-]+@[\w.-]+\.[A-Za-z]{2,}$/,
+              })}
               placeholder="Email"
               className="w-1/2 p-2 rounded-lg"
             />
+            {errors.email && (
+              <p className="text-rose-500 mt-1">
+                {errors.email.type === 'required' && 'This field is required.'}
+                {errors.email.type === 'pattern' && 'Invalid email address.'}
+              </p>
+            )}
           </div>
           <input
-            {...register('Subject')}
+            {...register('subject', { required: true })}
             placeholder="Subject"
             className="p-2 rounded-lg"
           />
+          {errors.subject && (
+            <p className="text-rose-500 mt-1">
+              {errors.subject.type === 'required' && 'This field is required.'}
+            </p>
+          )}
           <textarea
-            {...register('Message')}
+            {...register('message', { required: true, maxLength: 2000 })}
             placeholder="Type your message..."
             className="p-2 rounded-lg"
             rows="4"
             cols="50"
           />
+          {errors.message && (
+            <p className="text-rose-500 mt-1">
+              {errors.message.type === 'required' && 'This field is required.'}
+              {errors.message.type === 'maxLength' &&
+                'Max length is 2000 char.'}
+            </p>
+          )}
           <button type="submit"> Send</button>
         </form>
       </div>
@@ -53,29 +104,3 @@ export const Contact = () => {
     </section>
   );
 };
-
-// import { useState } from 'react';
-
-// export function Contact() {
-//   const [title, setTitle] = useState('');
-
-//   const handleChange = (event) => {
-//     setTitle(event.target.value);
-//   };
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-
-//     console.log(title);
-//     setTitle('');
-//   };
-//   return (
-//     <div id="contact">
-//       <h3> Add a book</h3>
-//       <form onSubmit={handleSubmit}>
-//         <label> Title</label>
-//         <input value={title} onChange={handleChange} />
-//         <button> Add</button>
-//       </form>
-//     </div>
-//   );
-// }
