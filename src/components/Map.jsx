a76093bb8c180de9a361e8186067e4f346eb4591
AvresCode <servas230@gmail.com>
2023-07-02 14:57:42 +0200

    fix marker showing problem

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,13 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 export const Map = () => {
   const myPosition = [52.0623013678065, 4.314696500970514];
